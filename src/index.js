c9710656c1b0354c7ab640a73c083f863ce334c5
Gourav Chakraborty <107490966+slaughterhectic@users.noreply.github.com>
2024-12-31 01:43:33 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style/main.css'
 import Application from './javascript/Application.js'
-
+import { Analytics } from "@vercel/analytics/react"
 window.application = new Application({
     $canvas: document.querySelector('.js-canvas'),
     useComposer: true
