b4f5204aa4be20867102f35541484d92122596a7
Gourav Chakraborty <your-email@example.com>
2025-12-27 03:21:49 +0530

    feat: integrate Vercel Analytics for usage tracking

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+import { inject } from '@vercel/analytics';
 import './style/main.css'
 import Application from './javascript/Application.js'
+
+inject();
 
 window.application = new Application({
     $canvas: document.querySelector('.js-canvas'),
